e9eb9dbc05dd7bf46500435bec9b0992dc1eeed7
bobby-lin <mr.linjiahao@gmail.com>
2023-12-20 07:28:03 +0800

    Add basic REPL

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for k, v := range getCommands() {
+		fmt.Println(k, ": ", v.description)
+	}
+
+	return nil
+}
+
+func commandExit() error {
+	return errors.New("exit")
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+	}
+}
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Print("Pokedex > ")
+	s := bufio.NewScanner(os.Stdin)
+	cmdMap := getCommands()
+
+	for s.Scan() {
+		commandName := s.Text()
+		c := cmdMap[commandName]
+		err := c.callback()
+
+		if err != nil {
+			break
+		}
+
+		fmt.Println()
+		fmt.Print("Pokedex > ")
+	}
 }
